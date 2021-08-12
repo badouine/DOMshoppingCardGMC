3351076e616708e0b28f5e21e4736db82b7c9057
badouine <nzbadouine@gmail.com>
2021-08-12 10:16:49 +0000

    test add cart

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
+let carts = document.querySelectorAll('.add-cart');
 
+for(let i=0;i < carts.length; i++){
+	carts[i].addEventListener('click', () => {
+		cartNumbers();
+	})
+}
+
+function onLoadCartNumbers(){
+	let productNumbers =  localStorage.getItem('cartNumbers');
+		if(productNumbers){
+			document.querySelector('.cart span').textContent = productNumbers;
+		}
+	}
+ 
+function cartNumbers(){
+	let productNumbers = localStorage.getItem('cartNumbers');
+	productNumbers = parseInt(productNumbers);
+
+	if(productNumbers){
+		localStorage.setItem('cartNumbers', productNumbers + 1);
+		document.querySelector('.cart span').textContent = productNumbers + 1;
+	}else{
+		localStorage.setItem('cartNumbers', 1);
+		document.querySelector('.cart span').textContent = 1;
+	}
+}
+
+onLoadCartNumbers();
 
 
 //  Write a function that replaces each integer with the next largest in the array.
